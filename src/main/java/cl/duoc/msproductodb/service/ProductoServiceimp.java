2da1c1f7a4a7694281cf83535cf9446b71f5c4ec
ana <serielain@hotmail.com>
2023-06-25 09:55:54 -0400

    ms-producto-db basedatos

---
+++ src/main/java/cl/duoc/msproductodb/service/ProductoServiceimp.java
@@ -0,0 +1,43 @@
+package cl.duoc.msproductodb.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import cl.duoc.msproductodb.model.entities.Producto;
+
+@Service
+public class ProductoServiceimp implements ProductoServicio {
+
+    @Autowired
+    private Producto producto;
+
+
+
+    @Override
+    public List<Producto> listaProductos() {
+        // metodo findall, para que recupere todos los objetos de tipo persona
+        //retornamos el LIST
+        return (List<Producto>) producto.findAll();
+
+    }
+
+    @Override
+    public void guardar(Producto producto) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void eliminar(Producto producto) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Producto encontrarProducto(Producto producto) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
